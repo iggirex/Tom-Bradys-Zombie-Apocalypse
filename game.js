0f76c7db048d4d70eff602411d43e300bcf7eab7
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-11-19 13:06:58 -0700

    added some game logic

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let territories = [];
+let territories = neighborHoods;
 
 let user = {
 	territories: [],
@@ -12,22 +12,25 @@ let zombies = {
 	points: 0
 };
 
+playGame();
+
 function playGame() {
 
 	territories = assignTerritories(territories);
-	setBoard();
+	setBoard(territories);
 
 	let currentPlayer = user;
 	let gameOver = false;
 
-	while(!gameOver) {
-		playerTurn(currentPlayer);
-		gameOver = currentPlayer.territories.length >= 22;
-		if(gameOver) break;
-		currentPlayer === user ? currentPlayer = zombies : currentPlayer = user;
-	}
-
-	announceWinner(currentPlayer);
+	// TODO Game Logig
+	// while(!gameOver) {
+	// 	playerTurn(currentPlayer);
+	// 	gameOver = currentPlayer.territories.length >= 22;
+	// 	if(gameOver) break;
+	// 	currentPlayer === user ? currentPlayer = zombies : currentPlayer = user;
+	// }
+	//
+	// announceWinner(currentPlayer);
 
 }
 
@@ -35,7 +38,7 @@ function assignTerritories(territories) {
 
 	let counter = 1;
 	let userTerritories = [];
-	let zombieTerritories = [];
+	let zombiesTerritories = [];
 
 	while (territories.length > 0) {
 
@@ -64,40 +67,53 @@ function assignTerritories(territories) {
 
 }
 
-function setBoard() {
+function setBoard(territories) {
 	for (let territory of territories) {
+
+		if (territory.owner == 'user') {
+			color = 'blue';
+		} else {
+			color = 'red'
+		}
+
+		let polygon = L.polygon([
+			[territory.coordinates[0][1], territory.coordinates[0][0]],
+			[territory.coordinates[1][1], territory.coordinates[1][0]],
+			[territory.coordinates[2][1], territory.coordinates[2][0]],
+			[territory.coordinates[3][1], territory.coordinates[3][0]]
+		], {color: color}).addTo(map)
 		// placeMarkers(territory);
 	}
 }
-
-// TODO placeMarkers(territories)
-
-function playerTurn(player) {
-
-	// Player spend cards
-
-	// Player place markers
-
-	// while (playerHasMoves()) {
-		// if playerPasses break
-		// SelectTerritory
-		// SelectTerritory to Attack
-		// SelectNumber of men to attack with
-		// Enter Battle
-		// Player chooses to end turn
+//
+// // TODO placeMarkers(territories)
+//
+// function playerTurn(player) {
+//
+// 	// Player spend cards
+//
+// 	// Player place markers
+//
+// 	// while (playerHasMoves()) {
+// 		// if playerPasses break
+// 		// SelectTerritory
+// 		// SelectTerritory to Attack
+// 		// SelectNumber of men to attack with
+// 		// Enter Battle
+// 		// Player chooses to end turn
+// // 	}
+//
+// 	// drawCard()
+// }
+//
+// function spendCards(player) {
+// 	if(player.points < 2) {
+// 		return
 // 	}
-
-	// drawCard()
-}
-
-function spendCards(player) {
-	if(player.points < 2) {
-		return
-	}
-	clearInstructions();
-
-}
-// TODO clearInstructions()
-function battle() {
-
-}
+// 	clearInstructions();
+//
+// }
+// // TODO clearInstructions()
+// function battle() {
+//
+// }
